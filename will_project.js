8963b6ac9aab248d57c744675deac4f28f76aede
fyq823 <yeqiufang2013@u.northwestern.edu>
2014-05-15 03:24:01 +0800

    Update will_project.js

--- will_project.js
+++ will_project.js
@@ -9,9 +9,67 @@ else
   xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
   }
 
+//------------------------------seed_data.js-----------------------------------
+var num_teams = 5; // Number of teams to generate
+var num_prefs = 3; // Number of preferences to generate
+var num_slots = 20;
+var team_list = []; //initialize empty array, this will be our output
+
+var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+//create one object per team
+for (var i = 0; i < num_teams; i++) { 
+
+  // initialize empty arrays, then push day and times
+  var pref_times = [];
+  var pref_days = [];
+
+  for (var j = 0; j < num_prefs; j++) {
+    var pref_time = Math.round(Math.random()*5) +5; //random night hour
+    var pref_day = days[Math.floor(Math.random()*days.length)]; //pick random day from list
+
+    // push to time and days array
+    pref_times.push(pref_time + " PM");
+    pref_days.push(pref_day);
+  }
+
+  //create the team object
+  var team = {
+        id: i +1, 
+        team_name: "Team_" + (i+1), 
+        pref_times: pref_times,
+        pref_days: pref_days
+        };
+
+  // push the object
+    team_list.push(team)
+}
+
+timeslot_list = []
+i = 1;
+for (var day = 0; day < 7; day ++) {
+  for (var time = 5; time < 10; time ++) {
+    for (var c = 1; c <7; c++) {
+      var slot = {
+            id: i,
+            time: time +" PM", 
+            day: days[day], 
+            court: "Court #" + c
+            };
+      timeslot_list.push(slot);
+      i ++;
+    };
+  };
+};
+
+// Turn array into json arrays
+team_list = JSON.parse(JSON.stringify(team_list));
+timeslot_list = JSON.parse(JSON.stringify(timeslot_list));
+
+//-------------------------------------------------seed_data.js over---------------------------------------------------
 
 
-
+//alert(JSON.stringify(team_list));
 /*xmlhttp.onreadystatechange=function()
   {
   if (xmlhttp.readyState)
@@ -21,10 +79,8 @@ else
   }*/
 
 //Json data need to be passed to php
-var json=
-'{"team":[{"name":"Brett","pref1":"1,1","pref2":"2,2","pref3":"3,3"},'+
-'{"name":"Jason","pref1":"4,4","pref2":"5,5","pref3":"6,6"},'+
-'{"name":"Elliotte","pref1":"7,7","pref2":"8,8","pref3":"9,9"}]}';
+var json=JSON.stringify(team_list);
+
 
 //Access php and pass json and a parameter: 0 request data, 1 send data
 xmlhttp.open("GET","will_project.php?p="+json+"&para=0",false);
