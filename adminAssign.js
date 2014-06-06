7d07aa2cc5eac01cf88e3597ca37b626cb54ca50
peterhad313 <peterhaddad2016@u.northwestern.edu>
2014-06-06 17:48:29 -0500

    Nested the database queries. Assign_Teams is now passed the correct values

--- adminAssign.js
+++ adminAssign.js
@@ -27,76 +27,68 @@ function adminAssign() {
     }
 
     timeslots = JSON.parse(JSON.stringify(timeslots));
-  });
-  //END TimeslotImport
+    var Teamdata = Parse.Object.extend("Teamdata");
+    // Prepare a query
+    var query = new Parse.Query(Teamdata);
+    // Find all the tuples in the table
+    query.equalTo("type", "team");
+    query.ascending("createdAt");
+    // Get the results set of the query
+    query.find().then(function(results) { //the results set can only be accessed in this function!!!
 
+      var teamlist = [];
 
-  //BEGIN Teams Import
-  // Select the table Teamdata in the database
-  var Teamdata = Parse.Object.extend("Teamdata");
-  // Prepare a query
-  var query = new Parse.Query(Teamdata);
-  // Find all the tuples in the table
-  query.equalTo("type", "team");
-  query.ascending("createdAt");
-  // Get the results set of the query
-  query.find().then(function(results) { //the results set can only be accessed in this function!!!
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+        var name = object.get("team_name");
+        var days = object.get("pref_days");
+        var times = object.get("pref_times");
+        var preflist = [];
+        j = 0;
+        while (times[j] != null) {
+          var day;
+          var time = times[j].slice(0, 1) - 5;
 
-    var teamlist = [];
+          switch (days[j]) {
+            case "Sunday":
+              day = 0;
+              break;
+            case "Monday":
+              day = 1;
+              break;
+            case "Tuesday":
+              day = 2;
+              break;
+            case "Wednesday":
+              day = 3;
+              break;
+            case "Thursday":
+              day = 4;
+              break;
+            default:
+              break;
+          }
 
-    for (var i = 0; i < results.length; i++) {
-      var object = results[i];
-      var name = object.get("team_name");
-      var days = object.get("pref_days");
-      var times = object.get("pref_times");
-      var preflist = [];
-      j = 0;
-      while (times[j] != null) {
-        var day;
-        var time = times[j].slice(0, 1) - 5;
-
-        switch (days[j]) {
-          case "Sunday":
-            day = 0;
-            break;
-          case "Monday":
-            day = 1;
-            break;
-          case "Tuesday":
-            day = 2;
-            break;
-          case "Wednesday":
-            day = 3;
-            break;
-          case "Thursday":
-            day = 4;
-            break;
-          default:
-            break;
+          var id = day * 5 + time;
+          preflist.push(timeslots[id]);
+          j++;
         }
+        var team = new Team(i + 1, name, preflist);
+        teamlist.push(team);
 
-        var id = day * 5 + time;
-        preflist.push(timeslots[id]);
-        j++;
       }
-      var team = new Team(i + 1, name, preflist);
-      teamlist.push(team);
 
-    }
-
-    teamlist = JSON.parse(JSON.stringify(teamlist));
-    //END team Import
-
-
-
-    Assign_Teams(teamlist, timeslots);
-    console.log("Assign_Teams succeeded");
-    var timeslot = new Time_Slot();
-    for (var x = 0; x < timeslots.length; x++) {
-      Games(timeslots[x]);
-    }
-  })
+      teamlist = JSON.parse(JSON.stringify(teamlist));
+      Assign_Teams(teamlist, timeslots);
+      console.log("Assign_Teams succeeded");
+      var timeslot = new Time_Slot();
+      for (var x = 0; x < timeslots.length; x++) {
+        Games(timeslots[x]);
+      }
+    });
+  });
 }
+
 
 function Assign_Teams(teaml, slotsl) {
   /**
