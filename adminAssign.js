406abdd241d8e9f2ad6bca8e58cb174ae57dfa43
Luke Olney <lukeolney@gmail.com>
2014-06-06 12:21:50 -0500

    Update adminAssign.js

--- adminAssign.js
+++ adminAssign.js
@@ -76,7 +76,7 @@ function Assign_Teams(teaml, slotsl) {
       if (!teaml[n].pref_slots[pref]) {
         alert("Team#: " + n + " and pref#: " + pref + "and number of prefs: " + prefs);
       }
-      var timeslotID = slotl.length
+      var timeslotID = slotsl.length
       var id = teaml[n].id;
       if (slotsl[id - 1].teams.length < slotsl[id - 1].capacity) {
         slotsl[id - 1].teams.push(teaml[n]);
