b3a4159a800752656dc0327550ea8256b9dcdce7
peterhad313 <peterhaddad2016@u.northwestern.edu>
2014-06-07 13:26:42 -0500

    dix adminAssign

--- adminAssign.js
+++ adminAssign.js
@@ -1,8 +1,7 @@
-
 /**
-* Takes all the teams and timeslots from the database
-* Calls Games to make the games for all timeslots
-*/
+ * Takes all the teams and timeslots from the database
+ * Calls Games to make the games for all timeslots
+ */
 function adminAssign() {
   // Insert the key to connect with the Parse system
   Parse.initialize("r3WndIFb85R0lx1qhchN4nquvAQVeKVrkA3TBnpI", "Wui7puCTZpnTmA5ZLvJmlj5R044vAyDerOBXhYzq");
@@ -18,84 +17,68 @@ function adminAssign() {
   var timeslots = new Array();
   query.find().then(function(results) { //the results set can only be accessed in this function!!!
 
-    for (var i = 0; i < results.length; i++) {
-      var object = results[i];
-      var day = object.get("day");
-      var time = object.get("time");
-      var courts = object.get("courts");
-      var timeslot = new Time_Slot(i, time, day, courts);
-      timeslots.push(timeslot);
-    }
-
-    timeslots = JSON.parse(JSON.stringify(timeslots));
-    var Teamdata = Parse.Object.extend("Teamdata");
-    // Prepare a query
-    var query = new Parse.Query(Teamdata);
-    // Find all the tuples in the table
-    query.equalTo("type", "team");
-    query.ascending("createdAt");
-    // Get the results set of the query
-    query.find().then(function(results) { //the results set can only be accessed in this function!!!
-
-      var teamlist = [];
-
       for (var i = 0; i < results.length; i++) {
         var object = results[i];
-        var name = object.get("team_name");
-        var days = object.get("pref_days");
-        var times = object.get("pref_times");
-        var preflist = [];
-        j = 0;
-        while (times[j] != null) {
-          var day;
-          var time = times[j].slice(0, 1) - 5;
+        var day = object.get("day");
+        var time = object.get("time");
+        var courts = object.get("courts");
+        var timeslot = new Time_Slot(i, time, day, courts);
+        timeslots.push(timeslot);
+      }
 
-          switch (days[j]) {
-            case "Sunday":
-              day = 0;
-              break;
-            case "Monday":
-              day = 1;
-              break;
-            case "Tuesday":
-              day = 2;
-              break;
-            case "Wednesday":
-              day = 3;
-              break;
-            case "Thursday":
-              day = 4;
-              break;
-            default:
-              break;
+      timeslots = JSON.parse(JSON.stringify(timeslots));
+      var Teamdata = Parse.Object.extend("Teamdata");
+      // Prepare a query
+      var query = new Parse.Query(Teamdata);
+      // Find all the tuples in the table
+      query.equalTo("type", "team");
+      query.ascending("createdAt");
+      // Get the results set of the query
+      query.find().then(function(results) { //the results set can only be accessed in this function!!!
+
+          var teamlist = [];
+
+          for (var i = 0; i < results.length; i++) {
+            var object = results[i];
+            var name = object.get("team_name");
+            var days = object.get("pref_days");
+            var times = object.get("pref_times");
+            var preflist = [];
+            j = 0;
+            while (times[j] != null) {
+              var day;
+              var time = times[j];
+              var xx = 0;
+              var check = 0; //check is if timeslot is found. xx interates through timeslots.
+              while (check == 0 && xx < timeslots.length) {}
+              if (days[j] == timeslots[xx].day && times[j] == timeslots[xx].time) { //if pref=timeslot
+                preflist.push(timeslots[xx]);
+                check = 1;
+              }
+              xx++;
+            }
+            j++;
           }
+          var team = new Team(i + 1, name, preflist);
+          teamlist.push(team);
 
-          var id = day * 5 + time;
-          preflist.push(timeslots[id]);
-          j++;
         }
-        var team = new Team(i + 1, name, preflist);
-        teamlist.push(team);
 
-      }
-
-      teamlist = JSON.parse(JSON.stringify(teamlist));
-      Assign_Teams(teamlist, timeslots);
-      console.log("Assign_Teams succeeded");
-      var timeslot = new Time_Slot();
-      for (var x = 0; x < timeslots.length; x++) {
-        Games(timeslots[x]);
-      }
-    });
+        teamlist = JSON.parse(JSON.stringify(teamlist)); Assign_Teams(teamlist, timeslots); console.log("Assign_Teams succeeded");
+        var timeslot = new Time_Slot();
+        for (var x = 0; x < timeslots.length; x++) {
+          Games(timeslots[x]);
+        }
+      });
   });
 }
 
 /**
-* Takes a list of timeslots and teams and assigns them according to their prefrences.
-* Populates the teams array in the timeslots based on the teams prefrences.
-* @param {Team} teamL                         List of teams, stored as an array
-* @param {Time_Slot} slotsL                   List of time slots, stored as an array
-*/
+ * Takes a list of timeslots and teams and assigns them according to their prefrences.
+ * Populates the teams array in the timeslots based on the teams prefrences.
+ * @param {Team} teamL                         List of teams, stored as an array
+ * @param {Time_Slot} slotsL                   List of time slots, stored as an array
+ */
 function Assign_Teams(teaml, slotsl) {
 
   n = 0;
@@ -119,7 +102,7 @@ function Assign_Teams(teaml, slotsl) {
         div.appendChild(text);
       }
       var xx = 0
-      while (xx < slotsl.length&&done==0) { // iterate through all time slots
+      while (xx < slotsl.length && done == 0) { // iterate through all time slots
         if (cteam.pref_slots[pref].day == slotsl[xx].day && cteam.pref_slots[pref].time == slotsl[xx].time && slotsl[xx].teams.length < slotsl[xx].capacity) { // check the timeslot is a prefrence. If so check if full
           slotsl[xx].teams.push(teaml[n]);
           teaml[n].timeslot = slotsl[xx];
@@ -131,7 +114,7 @@ function Assign_Teams(teaml, slotsl) {
     }
     n++;
   }
-  
+
   // Append div containing errors
   adminText.appendChild(div);
 
@@ -139,10 +122,10 @@ function Assign_Teams(teaml, slotsl) {
 
 
 /**
-* Takes a timeslot and matches the teams against eachother.
-* Matches 3-5 teams against each other over 5 weeks. Includes byes if nessecary
-* @param {Team} timeslot                      timeslot. Will access the group of teams
-*/
+ * Takes a timeslot and matches the teams against eachother.
+ * Matches 3-5 teams against each other over 5 weeks. Includes byes if nessecary
+ * @param {Team} timeslot                      timeslot. Will access the group of teams
+ */
 function Games(timeslot) {
   var numTeams = timeslot.teams.length;
   var errorString = "";
@@ -152,10 +135,10 @@ function Games(timeslot) {
   timeslot.games = [];
   if (numTeams < 3) { // need at least 4 teams to start. Otherwise, timeslot will not be used. 
     errorString = "Not Enough Teams in Timeslot. Only " + numTeams + " Teams";
-        var text = document.createElement("div");
-        text.innerHTML = errorString;
-        text.style.display = "block";
-        div.appendChild(text);
+    var text = document.createElement("div");
+    text.innerHTML = errorString;
+    text.style.display = "block";
+    div.appendChild(text);
   } else if (numTeams == 3) {
     timeslot.games[0] = new game(1, 1, 2, 1, 1)
     timeslot.games[1] = new game(2, 1, 3, 2, 1)
@@ -202,19 +185,19 @@ function Games(timeslot) {
     init(name, timeslot.games[x].court, "", s, e)
     //2014-05-19T20:00:00-06:00
   }
-  
-    adminText.appendChild(div);
+
+  adminText.appendChild(div);
 
 }
 
 
 /**
-* Finds the offset from the start date and uses this to find the date and time.
-* @param {int} week                     week the game is played RANGE[1-5]
-* @param {string} day                   day from the timeslot
-* @param {int} time                     time will be 5-8 but refers to PM
-* @return {string}                      returns the date and time in the format needed to add to the calendar
-*/
+ * Finds the offset from the start date and uses this to find the date and time.
+ * @param {int} week                     week the game is played RANGE[1-5]
+ * @param {string} day                   day from the timeslot
+ * @param {int} time                     time will be 5-8 but refers to PM
+ * @return {string}                      returns the date and time in the format needed to add to the calendar
+ */
 function getDateTime(week, day, time) {
 
   //date is the offset from April 6, the first day of games. 
@@ -242,7 +225,7 @@ function getDateTime(week, day, time) {
   //date all set
 
   //start time
-  int temp=time.slice(0,1)
-  dateString = dateString + ( temp + 12) + ":00:00-06:00"
+  int temp = time.slice(0, 1)
+  dateString = dateString + (temp + 12) + ":00:00-06:00"
   return dateString;
 }
